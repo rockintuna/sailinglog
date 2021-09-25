012362c369974a0b96cc75b9a1d90ce1d33fc4c4
rockintuna <rockintuna@naver.com>
2021-09-25 20:50:44 +0900

    add Test Case ArticleControllerTest.java  - getArticleById()  - createArticle()  - updateArticle()  - deleteArticleById()

--- src/test/java/me/rockintuna/sailinglog/controller/ArticleControllerTest.java
+++ src/test/java/me/rockintuna/sailinglog/controller/ArticleControllerTest.java
@@ -1,22 +1,26 @@
 package me.rockintuna.sailinglog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import me.rockintuna.sailinglog.domain.Article;
 import me.rockintuna.sailinglog.domain.ArticleRequestDto;
 import me.rockintuna.sailinglog.service.ArticleService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,23 +30,28 @@ class ArticleControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @MockBean
     private ArticleService articleService;
+
+    List<Article> articleList = new ArrayList<>();
+
+    @BeforeEach
+    private void beforeEach() {
+        //given
+        articleList.add(Article.from(
+                new ArticleRequestDto("test title 1", "tester", "test content 1")));
+        articleList.add(Article.from(
+                new ArticleRequestDto("test title 2", "tester", "test content 2")));
+        articleList.add(Article.from(
+                new ArticleRequestDto("test title 3", "tester", "test content 3")));
+    }
 
     @Test
     void getArticlesOrderByCreatedAtDesc() throws Exception {
         //given
-        ArticleRequestDto articleRequestDto1 =
-                new ArticleRequestDto("test title 1", "tester", "test content 1");
-        ArticleRequestDto articleRequestDto2 =
-                new ArticleRequestDto("test title 2", "tester", "test content 2");
-        ArticleRequestDto articleRequestDto3 =
-                new ArticleRequestDto("test title 3", "tester", "test content 3");
-        List<Article> articleList = new ArrayList<>();
-        articleList.add(Article.from(articleRequestDto1));
-        articleList.add(Article.from(articleRequestDto2));
-        articleList.add(Article.from(articleRequestDto3));
-
         given(articleService.getArticlesOrderByCreatedAtDesc())
                 .willReturn(articleList);
 
@@ -59,21 +68,77 @@ class ArticleControllerTest {
                 .andExpect(jsonPath("$[2].title").value("test title 3"))
                 .andExpect(jsonPath("$[2].writer").value("tester"))
                 .andExpect(jsonPath("$[2].content").value("test content 3"));
+
+        verify(articleService).getArticlesOrderByCreatedAtDesc();
     }
 
     @Test
-    void getArticleById() {
+    void getArticleById() throws Exception {
+        given(articleService.getArticleById(1L))
+                .willReturn(articleList.get(0));
+
+        mvc.perform(MockMvcRequestBuilders.get("/articles/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title").value("test title 1"))
+                .andExpect(jsonPath("$.writer").value("tester"))
+                .andExpect(jsonPath("$.content").value("test content 1"));
+
+        verify(articleService).getArticleById(1L);
     }
 
     @Test
-    void createArticle() {
+    void createArticle() throws Exception {
+        ArticleRequestDto requestDto =
+                new ArticleRequestDto("test title 1", "tester", "test content 1");
+        String jsonString = objectMapper.writeValueAsString(requestDto);
+
+        given(articleService.createArticle(any(ArticleRequestDto.class)))
+                .willReturn(articleList.get(0));
+
+        mvc.perform(post("/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.title").value("test title 1"))
+                .andExpect(jsonPath("$.writer").value("tester"))
+                .andExpect(jsonPath("$.content").value("test content 1"));
+
+        verify(articleService).createArticle(any(ArticleRequestDto.class));
     }
 
     @Test
-    void updateArticle() {
+    void updateArticle() throws Exception {
+        ArticleRequestDto requestDto =
+                new ArticleRequestDto("test title 1", "tester", "test content 1");
+        String jsonString = objectMapper.writeValueAsString(requestDto);
+
+        given(articleService.updateArticle(eq(1L), any(ArticleRequestDto.class)))
+                .willReturn(1L);
+
+        mvc.perform(MockMvcRequestBuilders.put("/articles/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonString))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
+
+        verify(articleService).updateArticle(eq(1L), any(ArticleRequestDto.class));
     }
 
     @Test
-    void deleteArticleById() {
+    void deleteArticleById() throws Exception {
+        given(articleService.deleteArticleById(1L))
+                .willReturn(1L);
+
+        mvc.perform(MockMvcRequestBuilders.delete("/articles/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
+
+        verify(articleService).deleteArticleById(1L);
     }
 }
