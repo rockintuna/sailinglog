7abcca1531da9b22c8d221a3ab97f071815a69cf
rockintuna <rockintuna@naver.com>
2021-09-26 16:40:45 +0900

    edit WebMvcConfig.java to get ObjectMapper bean.

--- src/main/java/me/rockintuna/sailinglog/config/WebMvcConfig.java
+++ src/main/java/me/rockintuna/sailinglog/config/WebMvcConfig.java
@@ -1,27 +1,23 @@
 package me.rockintuna.sailinglog.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
-@EnableWebMvc
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(htmlEscapingConverter());
-    }
+    private final ObjectMapper objectMapper;
 
-    private HttpMessageConverter<?> htmlEscapingConverter() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    @Bean
+    public HttpMessageConverter<?> htmlEscapingConverter() {
         objectMapper.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
-
         return new MappingJackson2HttpMessageConverter(objectMapper);
     }
 }
