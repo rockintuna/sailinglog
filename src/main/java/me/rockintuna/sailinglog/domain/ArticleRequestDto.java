44b34e5eec30820d572ad7d3d8562e918eb3695b
rockintuna <rockintuna@naver.com>
2021-09-29 15:10:21 +0900

    ArticleRequestDto 모든 필드 NonNull

--- src/main/java/me/rockintuna/sailinglog/domain/ArticleRequestDto.java
+++ src/main/java/me/rockintuna/sailinglog/domain/ArticleRequestDto.java
@@ -2,11 +2,15 @@ package me.rockintuna.sailinglog.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 
 @Getter
 @AllArgsConstructor
 public class ArticleRequestDto {
+    @NonNull
     private final String title;
+    @NonNull
     private final String writer;
+    @NonNull
     private final String content;
 }
