e4f09dd64cbd57b050e3d7c5becb373bd20e32cf
rockintuna <rockintuna@naver.com>
2021-10-02 15:39:38 +0900

    Account 클래스 생성

---
+++ src/main/java/me/rockintuna/sailinglog/model/Account.java
@@ -0,0 +1,32 @@
+package me.rockintuna.sailinglog.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Account extends Timestamped{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
