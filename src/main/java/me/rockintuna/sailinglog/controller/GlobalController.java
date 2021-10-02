ead92ff6dff671e559fd60d691debb318da40e8c
rockintuna <rockintuna@naver.com>
2021-10-02 20:15:42 +0900

    usernameExistHandler 핸들러

--- src/main/java/me/rockintuna/sailinglog/controller/GlobalController.java
+++ src/main/java/me/rockintuna/sailinglog/controller/GlobalController.java
@@ -1,6 +1,7 @@
 package me.rockintuna.sailinglog.controller;
 
 import me.rockintuna.sailinglog.config.exception.ArticleNotFoundException;
+import me.rockintuna.sailinglog.config.exception.UsernameExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +13,11 @@ public class GlobalController {
     @ExceptionHandler
     public ResponseEntity<String> articleNotFoundHandler(ArticleNotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> usernameExistHandler(UsernameExistException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 
 }
