43d4a414b97e35b8ee08051863e980b4256fc463
rockintuna <rockintuna@naver.com>
2021-10-02 15:54:06 +0900

    AccountController 메서드 생성

--- src/main/java/me/rockintuna/sailinglog/controller/AccountController.java
+++ src/main/java/me/rockintuna/sailinglog/controller/AccountController.java
@@ -1,10 +1,33 @@
 package me.rockintuna.sailinglog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/account")
 public class AccountController {
 
 
+    @GetMapping("/register")
+    public String registerPage() {
+        return null;
+    }
+
+    @PostMapping("/register")
+    public String register() {
+        return null;
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return null;
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return null;
+    }
 
 }
