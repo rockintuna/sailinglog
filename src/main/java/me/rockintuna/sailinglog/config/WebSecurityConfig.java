5cbf12c439841a139d17f0c7477d260a377de13a
rockintuna <rockintuna@naver.com>
2021-10-02 16:32:51 +0900

    WebSecurityConfig formLogin 설정 변경

--- src/main/java/me/rockintuna/sailinglog/config/WebSecurityConfig.java
+++ src/main/java/me/rockintuna/sailinglog/config/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package me.rockintuna.sailinglog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -11,9 +10,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll().and()
-                .csrf().ignoringAntMatchers("/account/**").and()
-                .formLogin().and()
-                .httpBasic();
+                    .csrf().ignoringAntMatchers("/account/**")
+                .and()
+                    .formLogin()
+                    .loginPage("/account/login")
+                    .loginProcessingUrl("/account/login")
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/account/login?error")
+                    .permitAll()
+                .and()
+                    .logout().logoutUrl("/account/logout")
+                .and()
+                    .httpBasic();
     }
 
 }
--- src/test/java/me/rockintuna/sailinglog/controller/AccountControllerTest.java
+++ src/test/java/me/rockintuna/sailinglog/controller/AccountControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(AccountController.class)
 class AccountControllerTest {
@@ -20,13 +21,15 @@ class AccountControllerTest {
     void getRegisterPage() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/account/register"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"));
     }
 
     @Test
     void register() throws Exception {
         mvc.perform(post("/account/register"))
                 .andDo(print())
+
                 .andExpect(status().is3xxRedirection());
     }
 
@@ -34,7 +37,8 @@ class AccountControllerTest {
     void getLoginPage() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/account/login"))
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
     }
 
     @Test
