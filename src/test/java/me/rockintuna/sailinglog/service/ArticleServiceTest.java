c7975e80cd5665a4d65ad3abc32f5c2e2f3b1a39
rockintuna <rockintuna@naver.com>
2021-10-04 17:38:52 +0900

    ArticleServiceTest 테스트 리팩터링

--- src/test/java/me/rockintuna/sailinglog/service/ArticleServiceTest.java
+++ src/test/java/me/rockintuna/sailinglog/service/ArticleServiceTest.java
@@ -7,6 +7,7 @@ import me.rockintuna.sailinglog.dto.ArticleRequestDto;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,6 +26,7 @@ import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("게시글 서비스")
 class ArticleServiceTest {
 
     @InjectMocks
@@ -48,114 +50,159 @@ class ArticleServiceTest {
                 ArticleRequestDto.of("test title 4", "tester", "<script>alert('XSS');</script>")));
     }
 
-    @Test
-    @DisplayName("게시글 목록 조회")
-    void getArticlesOrderByCreatedAtDesc() {
-        // given
-        given(articleRepository.findAllByOrderByCreatedAtDesc()).willReturn(mockArticleList);
 
-        // when
-        List<Article> articleList = articleService.getArticlesOrderByCreatedAtDesc();
+    @Nested
+    @DisplayName("게시글 조회")
+    class Read {
+        @Nested
+        @DisplayName("게시글 조회 성공")
+        class ReadSuccess {
+            @Test
+            @DisplayName("게시글 목록 조회")
+            void getArticlesOrderByCreatedAtDesc() {
+                // given
+                given(articleRepository.findAllByOrderByCreatedAtDesc()).willReturn(mockArticleList);
 
-        // then
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(articleList.size()).isEqualTo(mockArticleList.size());
-        softly.assertThat(articleList.get(0).getTitle()).isEqualTo(mockArticleList.get(0).getTitle());
-        softly.assertThat(articleList.get(0).getWriter()).isEqualTo(mockArticleList.get(0).getWriter());
-        softly.assertThat(articleList.get(0).getContent()).isEqualTo(mockArticleList.get(0).getContent());
-        softly.assertThat(articleList.get(3).getTitle()).isEqualTo(mockArticleList.get(3).getTitle());
-        softly.assertThat(articleList.get(3).getWriter()).isEqualTo(mockArticleList.get(3).getWriter());
-        softly.assertThat(articleList.get(3).getContent()).isEqualTo(mockArticleList.get(3).getContent());
-        softly.assertAll();
-        verify(articleRepository).findAllByOrderByCreatedAtDesc();
+                // when
+                List<Article> articleList = articleService.getArticlesOrderByCreatedAtDesc();
+
+                // then
+                SoftAssertions softly = new SoftAssertions();
+                softly.assertThat(articleList.size()).isEqualTo(mockArticleList.size());
+                softly.assertThat(articleList.get(0).getTitle()).isEqualTo(mockArticleList.get(0).getTitle());
+                softly.assertThat(articleList.get(0).getWriter()).isEqualTo(mockArticleList.get(0).getWriter());
+                softly.assertThat(articleList.get(0).getContent()).isEqualTo(mockArticleList.get(0).getContent());
+                softly.assertThat(articleList.get(3).getTitle()).isEqualTo(mockArticleList.get(3).getTitle());
+                softly.assertThat(articleList.get(3).getWriter()).isEqualTo(mockArticleList.get(3).getWriter());
+                softly.assertThat(articleList.get(3).getContent()).isEqualTo(mockArticleList.get(3).getContent());
+                softly.assertAll();
+                verify(articleRepository).findAllByOrderByCreatedAtDesc();
+            }
+
+            @Test
+            @DisplayName("게시글 ID로 조회")
+            void getArticleById() {
+                given(articleRepository.findById(1L))
+                        .willReturn(Optional.of(mockArticleList.get(0)));
+
+                Article article = articleService.getArticleById(1L);
+
+                SoftAssertions softly = new SoftAssertions();
+                softly.assertThat(article.getTitle()).isEqualTo(mockArticleList.get(0).getTitle());
+                softly.assertThat(article.getWriter()).isEqualTo(mockArticleList.get(0).getWriter());
+                softly.assertThat(article.getContent()).isEqualTo(mockArticleList.get(0).getContent());
+                softly.assertAll();
+                verify(articleRepository).findById(1L);
+            }
+        }
+        @Nested
+        @DisplayName("게시글 조회 실패")
+        class ReadFail {
+            @Test
+            @DisplayName("존재하지 않는 게시글 ID로 조회")
+            void getArticleByInvalidId() {
+                given(articleRepository.findById(99L))
+                        .willReturn(Optional.empty());
+
+                assertThatThrownBy(() -> articleService.getArticleById(99L))
+                        .isInstanceOf(ArticleNotFoundException.class);
+                verify(articleRepository).findById(99L);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("게시글 ID로 조회")
-    void getArticleById() {
-        given(articleRepository.findById(1L))
-                .willReturn(Optional.of(mockArticleList.get(0)));
-
-        Article article = articleService.getArticleById(1L);
-
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(article.getTitle()).isEqualTo(mockArticleList.get(0).getTitle());
-        softly.assertThat(article.getWriter()).isEqualTo(mockArticleList.get(0).getWriter());
-        softly.assertThat(article.getContent()).isEqualTo(mockArticleList.get(0).getContent());
-        softly.assertAll();
-        verify(articleRepository).findById(1L);
-    }
-
-    @Test
-    @DisplayName("존재하지 않는 게시글 ID로 조회")
-    void getArticleByInvalidId() {
-        given(articleRepository.findById(99L))
-                .willReturn(Optional.empty());
-
-        assertThatThrownBy(() -> articleService.getArticleById(99L))
-                .isInstanceOf(ArticleNotFoundException.class);
-        verify(articleRepository).findById(99L);
-    }
-
-    @Test
+    @Nested
     @DisplayName("게시글 생성")
-    void createArticle() {
-        ArticleRequestDto requestDto =
-                ArticleRequestDto.of("test title 1", "tester", "test content 1");
-        Article mockArticle = Article.from(requestDto);
-        given(articleRepository.save(any(Article.class))).willReturn(mockArticle);
+    class Create {
+        @Nested
+        @DisplayName("게시글 생성 성공")
+        class CreateSuccess {
+            @Test
+            @DisplayName("게시글 생성")
+            void createArticle() {
+                ArticleRequestDto requestDto =
+                        ArticleRequestDto.of("test title 1", "tester", "test content 1");
+                Article mockArticle = Article.from(requestDto);
+                given(articleRepository.save(any(Article.class))).willReturn(mockArticle);
 
-        Article savedArticle = articleService.createArticle(requestDto);
+                Article savedArticle = articleService.createArticle(requestDto);
 
-        SoftAssertions softly = new SoftAssertions();
-        softly.assertThat(savedArticle.getTitle()).isEqualTo(mockArticle.getTitle());
-        softly.assertThat(savedArticle.getWriter()).isEqualTo(mockArticle.getWriter());
-        softly.assertThat(savedArticle.getContent()).isEqualTo(mockArticle.getContent());
-        softly.assertAll();
-        verify(articleRepository).save(any(Article.class));
+                SoftAssertions softly = new SoftAssertions();
+                softly.assertThat(savedArticle.getTitle()).isEqualTo(mockArticle.getTitle());
+                softly.assertThat(savedArticle.getWriter()).isEqualTo(mockArticle.getWriter());
+                softly.assertThat(savedArticle.getContent()).isEqualTo(mockArticle.getContent());
+                softly.assertAll();
+                verify(articleRepository).save(any(Article.class));
+            }
+        }
+        @Nested
+        @DisplayName("게시글 생성 실패")
+        class CreateFail {
+
+        }
     }
+    @Nested
+    @DisplayName("게시글 수정")
+    class Update {
+        @Nested
+        @DisplayName("게시글 수정 성공")
+        class UpdateSuccess {
+            @Test
+            @DisplayName("게시글 ID로 게시글 수정")
+            void updateArticle() {
+                ArticleRequestDto requestDto =
+                        ArticleRequestDto.of("updated title 1", "tester", "updated content 1");
 
-    @Test
-    @DisplayName("게시글 ID로 게시글 수정")
-    void updateArticle() {
-        ArticleRequestDto requestDto =
-                ArticleRequestDto.of("updated title 1", "tester", "updated content 1");
+                given(articleRepository.findById(3L))
+                        .willReturn(Optional.of(mockArticleList.get(0)));
 
-        given(articleRepository.findById(3L))
-                .willReturn(Optional.of(mockArticleList.get(0)));
+                articleService.updateArticle(3L, requestDto);
+                verify(articleRepository).save(any(Article.class));
+            }
+        }
+        @Nested
+        @DisplayName("게시글 수정 실패")
+        class UpdateFail {
+            @Test
+            @DisplayName("존재하지 않는 게시글 ID로 수정")
+            void updateArticleByInvalidId() {
+                ArticleRequestDto requestDto =
+                        ArticleRequestDto.of("updated title 1", "tester", "updated content 1");
+                given(articleRepository.findById(99L))
+                        .willReturn(Optional.empty());
 
-        articleService.updateArticle(3L, requestDto);
-        verify(articleRepository).save(any(Article.class));
+                assertThatThrownBy(() -> articleService.updateArticle(99L, requestDto))
+                        .isInstanceOf(ArticleNotFoundException.class);
+                verify(articleRepository).findById(99L);
+            }
+        }
     }
+    @Nested
+    @DisplayName("게시글 삭제")
+    class Delete {
+        @Nested
+        @DisplayName("게시글 삭제 성공")
+        class DeleteSuccess {
+            @Test
+            @DisplayName("게시글 ID로 게시글 삭제")
+            void deleteArticleById() {
+                articleService.deleteArticleById(3L);
+                verify(articleRepository).deleteById(3L);
+            }
+        }
+        @Nested
+        @DisplayName("게시글 삭제 실패")
+        class DeleteFail {
+            @Test
+            @DisplayName("존재하지 않는 게시글 ID로 삭제")
+            void deleteArticleByInvalidId() {
+                willThrow(EmptyResultDataAccessException.class)
+                        .given(articleRepository).deleteById(99L);
 
-    @Test
-    @DisplayName("존재하지 않는 게시글 ID로 수정")
-    void updateArticleByInvalidId() {
-        ArticleRequestDto requestDto =
-                ArticleRequestDto.of("updated title 1", "tester", "updated content 1");
-        given(articleRepository.findById(99L))
-                .willReturn(Optional.empty());
-
-        assertThatThrownBy(() -> articleService.updateArticle(99L, requestDto))
-                .isInstanceOf(ArticleNotFoundException.class);
-        verify(articleRepository).findById(99L);
-    }
-
-    @Test
-    @DisplayName("게시글 ID로 게시글 삭제")
-    void deleteArticleById() {
-        articleService.deleteArticleById(3L);
-        verify(articleRepository).deleteById(3L);
-    }
-
-    @Test
-    @DisplayName("존재하지 않는 게시글 ID로 삭제")
-    void deleteArticleByInvalidId() {
-        willThrow(EmptyResultDataAccessException.class)
-                .given(articleRepository).deleteById(99L);
-
-        assertThatThrownBy(() -> articleService.deleteArticleById(99L))
-                .isInstanceOf(ArticleNotFoundException.class);
-        verify(articleRepository).deleteById(99L);
+                assertThatThrownBy(() -> articleService.deleteArticleById(99L))
+                        .isInstanceOf(ArticleNotFoundException.class);
+                verify(articleRepository).deleteById(99L);
+            }
+        }
     }
 }
